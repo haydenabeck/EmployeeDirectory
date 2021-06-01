2e63d0287384821bea693b1b81e99b7ebc49abf9
Hayden Beck <haydenabeck@gmail.com>
2021-06-01 18:48:04 -0500

    added app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Table from "./components/Table.js";
+
+function App() {
+  return (
+    <div>
+      <Table />
+      {/* OTHER WHOLE COMPONENTS */}
+    </div>
+  );
+}
+
+export default App;
