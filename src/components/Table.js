7cb2479f7346441bf22087cd381aae75744aa7e0
Hayden Beck <haydenabeck@gmail.com>
2021-06-04 15:00:09 -0500

    sort and filter added

--- src/components/Table.js
+++ src/components/Table.js
@@ -15,14 +15,49 @@ function Table() {
     console.log(...randomUser);
   };
 
+  const sortAlphabetically = (e) => {
+    function compare(a, b) {
+      if (a.name.first < b.name.first) {
+        return -1;
+      }
+      if (a.name.first > b.name.first) {
+        return 1;
+      }
+      return 0;
+    }
+
+    var sortedUser = randomUser.sort(compare);
+
+    setRandomUser([...sortedUser]);
+  };
+
+  const filterAge = (e) => {
+    var peopleFifty = [];
+    for (let index = 0; index < randomUser.length; index++) {
+      const element = randomUser[index];
+
+      if (randomUser[index].dob.age > 50) {
+        peopleFifty.push(randomUser[index]);
+      }
+    }
+
+    // var sortedUser = randomUser.sort(compare);
+
+    setRandomUser([...peopleFifty]);
+  };
+
   return (
     <table className="table">
       <thead>
         <tr>
           <th scope="col">Photo</th>
-          <th scope="col">First</th>
+          <th scope="col">
+            <button onClick={sortAlphabetically}>First</button>
+          </th>
           <th scope="col">Last</th>
-          <th scope="col">Age</th>
+          <th scope="col">
+            <button onClick={filterAge}>Age</button>
+          </th>
           <th scope="col">Gender</th>
           <th scope="col">Location</th>
           <th scope="col">Email</th>
